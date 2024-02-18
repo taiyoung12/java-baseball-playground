b05b53a138392b90ced49b2e8004c7369fb4e759
taiyoung <qq3222@naver.com>
2024-02-18 21:41:35 +0900

    feat: 게임 종료 후 1이란 숫자를 입력 받았을 때 게임을 재시작할 수 있다. 게임 종료 후 2란 숫자를 입력 받았을 때 게임을 종료할 수 있다.

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -2,16 +2,38 @@ package baseball;
 
 public class Main {
     public static void main(String[] args) {
-        BaseBall baseBall = new BaseBall();
-        String answer = baseBall.generateAnswer();
+        while (true) {
+            BaseBall baseBall = new BaseBall();
+            String answer = baseBall.generateAnswer();
 
-        InputView input = new InputView();
-        String status;
+            InputView input = new InputView();
+            ResultView result = new ResultView();
+            String status;
 
-        do {
-            String userInput = input.getUserInput();
-            status = baseBall.validateAnswer(userInput, answer);
-            System.out.println(status);
-        } while (!"3스트라이크 0볼".equals(status));
+            do {
+                String userInput = input.getUserInput();
+                status = baseBall.validateAnswer(userInput, answer);
+                System.out.println(status);
+            } while (!"3스트라이크 0볼".equals(status));
+
+            if(status.equals("3스트라이크 0볼")){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
+
+            String resultInput;
+
+            do {
+                resultInput = result.getUserResultInput();
+                if ("1".equals(resultInput)) {
+                    System.out.println("게임을 새로 시작합니다.");
+                    break;
+                }
+
+                if("2".equals(resultInput)) {
+                    System.out.println("게임을 종료합니다.");
+                    return;
+                }
+            } while (!"1".equals(resultInput) && !"2".equals(resultInput));
+        }
     }
 }
--- src/main/java/baseball/ResultView.java
+++ src/main/java/baseball/ResultView.java
@@ -1,4 +1,18 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class ResultView {
+    private Scanner scanner;
+    public ResultView(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getUserResultInput(){
+        String input;
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        input = scanner.nextLine();
+
+        return input;
+    }
 }
